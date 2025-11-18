3a97b09e254b59413ba83ae45fb885cc9ad26ef7
Akshay Jagtap <jagtapabhay82@gmail.com>
2025-11-18 20:02:07 +0530

    Controller, Database tables and Models created with migration and tinker

---
+++ app/Models/Chirp.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Chirp extends Model
+{
+    protected $fillable = [
+        'message',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $thi->belongsTo(User::class);
+    }
+}
